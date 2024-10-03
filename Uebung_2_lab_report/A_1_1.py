4ba1d0f3189e02a55ff027e1f525c082f3a88370
Luna-Schaetzle <luna.schaetzle@gmail.com>
2024-10-03 13:32:48 +0200

    save statistik as png

---
+++ Uebung_2_lab_report/A_1_1.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+#source: https://www.kaggle.com/datasets/emmanuelfwerr/london-weather-data
+d = np.genfromtxt('/home/luna/5BHWII/INFI_Informations_Systeme/Uebung_2_lab_report/data/london weather.csv', delimiter=",", skip_header=1 )
+
+dt =  d[:,0] #Datum mit folgendem Aufbau: 19790103 (3.Jänner 1979)
+# Aufteilen in Tag, Monat, Jahr
+day = (dt % 100).astype('i')
+month = (dt % 10000 / 100).astype('i')
+year = (dt % 100000000 / 10000).astype('i')
+
+
+# Darstellung der Temperaturunterschiede
+# Aktuell interessant ist, wie sich die Temperatur über die Jahre verändert hat. zu diesem Zweck einfach 4
+# aussagekräftige Jahre aussuchen und diese Unterschiede deutlich machen, am Besten als Boxplots.
+
+mean_temp_1980 = np.mean(d[year == 1980, 6])
+mean_temp_1990 = np.mean(d[year == 1990, 6])
+mean_temp_2000 = np.mean(d[year == 2000, 6])
+mean_temp_2010 = np.mean(d[year == 2010, 6])
+
+print("Mean Temp 1980:", mean_temp_1980)
+print("Mean Temp 1990:", mean_temp_1990)
+print("Mean Temp 2000:", mean_temp_2000)
+print("Mean Temp 2010:", mean_temp_2010)
+
+# Gegenüberstellung der Mitteltemperaturen
+plt.boxplot([d[year == 1980, 6], d[year == 1990, 6], d[year == 2000, 6], d[year == 2010, 6]])
+plt.xticks([1,2,3,4], ["1980", "1990", "2000", "2010"])
+plt.ylabel("Temperature in °C")
+plt.title("Temperaturen in London")
+plt.grid()
+plt.savefig("/home/luna/5BHWII/INFI_Informations_Systeme/Uebung_2_lab_report/A_1_1.png")
+plt.show()
+
+
+
+
---
+++ Uebung_2_lab_report/A_1_2.py
@@ -0,0 +1,28 @@
+# Eine Temperaturkurve für ein beliebiges Jahr soll erstellt werden, vorzugsweise als Punktdiagramm
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+#source: https://www.kaggle.com/datasets/emmanuelfwerr/london-weather-data
+d = np.genfromtxt('/home/luna/5BHWII/INFI_Informations_Systeme/Uebung_2_lab_report/data/london weather.csv', delimiter=",", skip_header=1 )
+
+print("Welches Jahr soll dargestellt werden?")
+year_input = int(input())
+
+plt.title("Mean temperature of every day in the year " + str(year_input))
+plt.xlabel("Day")
+plt.ylabel("Mean Temperature")
+day = 0
+# mean temperature of every day in the year in a point diagram
+for i in range(1, len(d)):
+    year = (d[i,0] % 100000000 / 10000).astype('i')
+    if year == year_input:
+        day = day + 1
+        mean_temp = d[i, 6]
+        print("Day:", day, "Mean Temp:", mean_temp)
+        plt.plot(day, mean_temp, "r.")
+
+plt.savefig("A_1_2.png")
+plt.show()
+
+    
