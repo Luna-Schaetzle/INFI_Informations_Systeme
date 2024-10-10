5e3f51138723f637699fcc2f4707d5fe2f83180a
Luna-Schaetzle <luna.schaetzle@gmail.com>
2024-10-10 18:38:25 +0200

    Aufgabenauswertung in pdf

--- Uebung_2_lab_report/A_1_5.py
+++ Uebung_2_lab_report/A_1_5.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -7,21 +5,20 @@ import matplotlib.pyplot as plt
 file_path = '/home/luna/5BHWII/INFI_Informations_Systeme/Uebung_2_lab_report/data/london weather.csv'
 weather_data = pd.read_csv(file_path)
 
-# Wir gehen ein Jahr durch und speichern uns die Mitteltemperaturen jedes Tages
-# Als erstes gehen wir die Jahre durch und speichern uns die Mitteltemperaturen
-years = weather_data['date'] // 10000
-unique_years = years.unique()
+# Extrahiere die Jahre aus der Datenspalte
+weather_data['year'] = weather_data['date'] // 10000
+unique_years = weather_data['year'].unique()
 
-# Wir untersuchen die veränderung des niederschlages "percipitation" über die Jahre
-percipitation = []
+# Berechne den durchschnittlichen Niederschlag pro Jahr
+avg_precipitation = []
 for year in unique_years:
-    percipitation.append(weather_data[years == year]['precipitation'].dropna().tolist())
+    avg_precipitation.append(weather_data[weather_data['year'] == year]['precipitation'].dropna().mean())
 
-# Nun erstellen wir ein lienien diagramm für die Niederschläge der letzten 10 Jahre
-plt.plot(unique_years, percipitation)
-plt.title("Niederschläge der letzten 10 Jahre in London")
+# Erstelle ein Liniendiagramm für den durchschnittlichen Niederschlag pro Jahr
+plt.plot(unique_years, avg_precipitation)
+plt.title("Durchschnittliche Niederschläge der letzten Jahre in London")
 plt.xlabel("Jahr")
-plt.ylabel("Niederschlag in mm")
+plt.ylabel("Durchschnittlicher Niederschlag in mm")
 plt.grid()
 plt.savefig("A_1_5.png")
 plt.show()
